1400c6a7a077069085c596f47943ec41a991fab8
ibktommy <atomquartz@gmail.com>
2022-06-28 12:27:57 +0100

    Styling of Login Page for desktop view

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Login.scss";
 import Logo from "../../images/logo.png";
 import loginImage from "../../images/pablo-sign-in 1.png";
@@ -8,29 +7,31 @@ const Login = () => {
 	return (
 		<>
 			<div className="container">
-				<header>
-					<img src={Logo} alt="logo" />
-				</header>
+				<div className="container-body">
+					<header>
+						<img src={Logo} alt="logo" />
+					</header>
 
-				<div className="content">
-					<div className="left">
-						<img src={loginImage} alt="loginImage" />
-					</div>
-
-					<div className="right">
-						<div className="header">
-							<h1 className="title">Welcome!</h1>
-							<p>Enter details to login.</p>
+					<div className="content row-flex">
+						<div className="left">
+							<img src={loginImage} alt="loginImage" />
 						</div>
 
-						<form>
-							<input type="email" placeholder="Email" />
-							<input type="password" placeholder="Password" />
+						<div className="right">
+							<div className="header">
+								<h1 className="title">Welcome!</h1>
+								<p>Enter details to login.</p>
+							</div>
 
-							<Link to="#">Forgot Password?</Link>
+							<form className="col-flex">
+								<input type="email" placeholder="Email" />
+								<input type="password" placeholder="Password" />
 
-							<button type="submit">Log In</button>
-						</form>
+								<p className="link">Forgot Password?</p>
+
+								<button type="submit">Log In</button>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
