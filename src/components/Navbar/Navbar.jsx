066c23997cbd00de76bef79d1c971a6800080f0f
ibktommy <atomquartz@gmail.com>
2022-06-28 16:59:03 +0100

    Styling of Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import "./Navbar.scss";
 import Logo from "../../images/login/logo.png";
+import SearchIcon from "../../images/navbar-icons/search-icon.png";
+import NotificationIcon from "../../images/navbar-icons/notificationBell.png";
+import User from "../../images/navbar-img/image 4 (1).png";
+import DownArrowIcon from "../../images/navbar-icons/down-arrow.png";
 
 const Navbar = () => {
 	return (
 		<>
-			<div className="navbar">
+			<div className="navbar row-flex">
 				<div className="logo">
 					<img src={Logo} alt="logo" />
+				</div>
+
+				<div className="search-bar row-flex">
+					<input type="search" placeholder="Search for anything" />
+					<button>
+						<img src={SearchIcon} alt="searchIcon" />
+					</button>
+				</div>
+
+				<div className="options row-flex">
+					<p className="link">Docs</p>
+					<img src={NotificationIcon} alt="notification-bell" />
+					<div className="user row-flex">
+						<img src={User} alt="User" />
+						<p className="username">Adedeji</p>
+						<img
+							src={DownArrowIcon}
+							alt="down-Arrow-Icon"
+							className="user-options"
+						/>
+					</div>
 				</div>
 			</div>
 		</>
