a89781dccb67e63b629f929d786eb35d21e5eead
ibktommy <atomquartz@gmail.com>
2022-06-29 11:25:29 +0100

    making the Sidebar responsive

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideBar.scss";
 import BriefcaseIcon from "../../images/sidebar-icons/png/briefcase 1.png";
 import DownArrowIcon from "../../images/sidebar-icons/png/downArrow.png";
@@ -23,119 +23,141 @@ import BadgeIcon from "../../images/sidebar-icons/png/badge-percent 1.png";
 import ClipBoardIcon from "../../images/sidebar-icons/png/clipboard-list 1.png";
 
 const SideBar = () => {
+	// State for Menu Button
+	const [menuBtn, setMenuBtn] = useState(false);
+
+	function toggle() {
+		setMenuBtn(!menuBtn);
+	}
+
 	return (
 		<>
 			<div className="sidebar">
-				<div className="sidebar-content col-flex">
-					<div className="sidebar-content_top-options col-flex">
-						<div className="sidebar-options row-flex">
-							<img src={BriefcaseIcon} alt="briefcase" />
-							<h4 className="sidebar-options-title">Switch Organization</h4>
-							<img src={DownArrowIcon} alt="downArrow" />
-						</div>
-						<div className="sidebar-options row-flex">
-							<img src={HomeIcon} alt="briefcase" />
-							<h4 className="sidebar-options-title">Dashboard</h4>
-						</div>
+				<div
+					className={menuBtn ? "sidebar-menu-item menu" : "sidebar-menu-item"}
+				>
+					<div className="sidebar-menu-item_bar" onClick={toggle}>
+						<i className={menuBtn ? "fas fa-bars" : "fas fa-bars menu"}></i>
+						<i className={menuBtn ? "fas fa-times" : "fas fa-times menu"}></i>
 					</div>
 
-					<div className="sidebar-content_other-options col-flex">
-						<div className="sidebar-content_other-options_box col-flex">
-							<h3 className="other-options-title">customers</h3>
-							<ul className="other-options-list col-flex">
-								<li className="sidebar-options">
-									<img src={UserFriendsIcon} alt="user-friends" />
-									<h4 className="sidebar-options-title">Users</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={UserIcon} alt="users" />
-									<h4 className="sidebar-options-title">guarantors</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={SackIcon} alt="SackIcon" />
-									<h4 className="sidebar-options-title">loans</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={HandShakeIcon} alt="HandShakeIcon" />
-									<h4 className="sidebar-options-title">decision models</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={PiggyBankIcon} alt="PiggyBankIcon" />
-									<h4 className="sidebar-options-title">savings</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={HandSackIcon} alt="HandSackIcon" />
-									<h4 className="sidebar-options-title">loan requests</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={UserCheckIcon} alt="UserCheckIcon" />
-									<h4 className="sidebar-options-title">whitelist</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={UsersTimesIcon} alt="UsersTimesIcon" />
-									<h4 className="sidebar-options-title">karma</h4>
-								</li>
-							</ul>
+					<div
+						className={
+							!menuBtn
+								? "sidebar-content menu col-flex"
+								: "sidebar-content col-flex"
+						}
+					>
+						<div className="sidebar-content_top-options col-flex">
+							<div className="sidebar-options row-flex">
+								<img src={BriefcaseIcon} alt="briefcase" />
+								<h4 className="sidebar-options-title">Switch Organization</h4>
+								<img src={DownArrowIcon} alt="downArrow" />
+							</div>
+							<div className="sidebar-options row-flex">
+								<img src={HomeIcon} alt="briefcase" />
+								<h4 className="sidebar-options-title">Dashboard</h4>
+							</div>
 						</div>
 
-						<div className="sidebar-content_other-options_box col-flex">
-							<h3 className="other-options-title">Businesses</h3>
-							<ul className="other-options-list col-flex">
-								<li className="sidebar-options">
-									<img src={BriefcaseIcon} alt="BriefcaseIcon" />
-									<h4 className="sidebar-options-title">organization</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={HandSackIcon} alt="HandSackIcon" />
-									<h4 className="sidebar-options-title">loan products</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={BankIcon} alt="BankIcon" />
-									<h4 className="sidebar-options-title">savings products</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={CoinsIcon} alt="CoinsIcon" />
-									<h4 className="sidebar-options-title">Fees and Charges</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={TransferIcon} alt="TransferIcon" />
-									<h4 className="sidebar-options-title">Transactions</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={ServicesIcon} alt="HandSackIcon" />
-									<h4 className="sidebar-options-title">Services</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={UsersServicesIcon} alt="UserCheckIcon" />
-									<h4 className="sidebar-options-title">services account</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={ScrollIcon} alt="ScrollIcon" />
-									<h4 className="sidebar-options-title">settlements</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={ChartBarIcon} alt="ScrollIcon" />
-									<h4 className="sidebar-options-title">reports</h4>
-								</li>
-							</ul>
-						</div>
+						<div className="sidebar-content_other-options col-flex">
+							<div className="sidebar-content_other-options_box col-flex">
+								<h3 className="other-options-title">customers</h3>
+								<ul className="other-options-list col-flex">
+									<li className="sidebar-options">
+										<img src={UserFriendsIcon} alt="user-friends" />
+										<h4 className="sidebar-options-title">Users</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={UserIcon} alt="users" />
+										<h4 className="sidebar-options-title">guarantors</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={SackIcon} alt="SackIcon" />
+										<h4 className="sidebar-options-title">loans</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={HandShakeIcon} alt="HandShakeIcon" />
+										<h4 className="sidebar-options-title">decision models</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={PiggyBankIcon} alt="PiggyBankIcon" />
+										<h4 className="sidebar-options-title">savings</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={HandSackIcon} alt="HandSackIcon" />
+										<h4 className="sidebar-options-title">loan requests</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={UserCheckIcon} alt="UserCheckIcon" />
+										<h4 className="sidebar-options-title">whitelist</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={UsersTimesIcon} alt="UsersTimesIcon" />
+										<h4 className="sidebar-options-title">karma</h4>
+									</li>
+								</ul>
+							</div>
 
-						<div className="sidebar-content_other-options_box col-flex">
-							<h3 className="other-options-title">Settings</h3>
-							<ul className="other-options-list col-flex">
-								<li className="sidebar-options">
-									<img src={SliderIcon} alt="SliderIcon" />
-									<h4 className="sidebar-options-title">organization</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={BadgeIcon} alt="BadgeIcon" />
-									<h4 className="sidebar-options-title">loan products</h4>
-								</li>
-								<li className="sidebar-options">
-									<img src={ClipBoardIcon} alt="ClipBoardIcon" />
-									<h4 className="sidebar-options-title">savings products</h4>
-								</li>
-							</ul>
+							<div className="sidebar-content_other-options_box col-flex">
+								<h3 className="other-options-title">Businesses</h3>
+								<ul className="other-options-list col-flex">
+									<li className="sidebar-options">
+										<img src={BriefcaseIcon} alt="BriefcaseIcon" />
+										<h4 className="sidebar-options-title">organization</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={HandSackIcon} alt="HandSackIcon" />
+										<h4 className="sidebar-options-title">loan products</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={BankIcon} alt="BankIcon" />
+										<h4 className="sidebar-options-title">savings products</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={CoinsIcon} alt="CoinsIcon" />
+										<h4 className="sidebar-options-title">Fees and Charges</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={TransferIcon} alt="TransferIcon" />
+										<h4 className="sidebar-options-title">Transactions</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={ServicesIcon} alt="HandSackIcon" />
+										<h4 className="sidebar-options-title">Services</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={UsersServicesIcon} alt="UserCheckIcon" />
+										<h4 className="sidebar-options-title">services account</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={ScrollIcon} alt="ScrollIcon" />
+										<h4 className="sidebar-options-title">settlements</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={ChartBarIcon} alt="ScrollIcon" />
+										<h4 className="sidebar-options-title">reports</h4>
+									</li>
+								</ul>
+							</div>
+
+							<div className="sidebar-content_other-options_box col-flex">
+								<h3 className="other-options-title">Settings</h3>
+								<ul className="other-options-list col-flex">
+									<li className="sidebar-options">
+										<img src={SliderIcon} alt="SliderIcon" />
+										<h4 className="sidebar-options-title">organization</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={BadgeIcon} alt="BadgeIcon" />
+										<h4 className="sidebar-options-title">loan products</h4>
+									</li>
+									<li className="sidebar-options">
+										<img src={ClipBoardIcon} alt="ClipBoardIcon" />
+										<h4 className="sidebar-options-title">savings products</h4>
+									</li>
+								</ul>
+							</div>
 						</div>
 					</div>
 				</div>
