61091fa7158ecd4ea01e92b18cf7f7ae1b23023b
ibktommy <atomquartz@gmail.com>
2022-06-29 19:13:04 +0100

    Working on Responsive Design of UserDetails

--- src/components/UserGenDetailsContent/UserGenDetailsContent.jsx
+++ src/components/UserGenDetailsContent/UserGenDetailsContent.jsx
@@ -63,67 +63,145 @@ const UserGenDetailsContent = () => {
 
 				<div className="main-content">
 					<div className="main-content_box col-flex">
-						<h2 className="heading-bold">
-							Personal Information
-						</h2>
+						<h2 className="heading-bold">Personal Information</h2>
 
 						<div className="contain row-flex">
 							<div className="main-content_box-details col-flex">
 								<div className="table">
-									<h3 className="main-content_box-details heading-light">
-										Full name
-									</h3>
+									<h3 className=" heading-light">Full name</h3>
 									<p className="para">Grace Effiom</p>
 								</div>
 								<div className="table">
-									<h3 className="main-content_box-details heading-light">
-										Marital Status
-									</h3>
+									<h3 className=" heading-light">Marital Status</h3>
 									<p className="para">Single</p>
 								</div>
 							</div>
 							<div className="main-content_box-details col-flex">
 								<div className="table">
-									<h3 className="main-content_box-details heading-light">
-										Phone Number
-									</h3>
+									<h3 className=" heading-light">Phone Number</h3>
 									<p className="para">07060780922</p>
 								</div>
 								<div className="table">
-									<h3 className="main-content_box-details heading-light">
-										Children
-									</h3>
+									<h3 className=" heading-light">Children</h3>
 									<p className="para">None</p>
 								</div>
 							</div>
 							<div className="main-content_box-details col-flex">
 								<div className="table">
-									<h3 className="main-content_box-details heading-light">
-										Email Address
-									</h3>
+									<h3 className=" heading-light">Email Address</h3>
 									<p className="para">grace@gmail.com</p>
 								</div>
 								<div className="table">
-									<h3 className="main-content_box-details heading-light">
-										Type of residence
-									</h3>
+									<h3 className=" heading-light">Type of residence</h3>
 									<p className="para">Parent’s Apartment</p>
 								</div>
 							</div>
 							<div className="main-content_box-details col-flex">
 								<div className="table">
-									<h3 className="main-content_box-details heading-light">
-										Bvn
-									</h3>
+									<h3 className=" heading-light">Bvn</h3>
 									<p className="para">07060780922</p>
 								</div>
 							</div>
 							<div className="main-content_box-details col-flex">
 								<div className="table">
-									<h3 className="main-content_box-details heading-light">
-										Gender
-									</h3>
+									<h3 className=" heading-light">Gender</h3>
 									<p className="para">Female</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="main-content_box col-flex">
+						<h2 className="heading-bold">Education and Employment</h2>
+
+						<div className="contain row-flex">
+							<div className="main-content_box-details col-flex">
+								<div className="table">
+									<h3 className=" heading-light">level of education</h3>
+									<p className="para">B.Sc</p>
+								</div>
+								<div className="table">
+									<h3 className=" heading-light">office email</h3>
+									<p className="para">grace@lendsqr.com</p>
+								</div>
+							</div>
+							<div className="main-content_box-details col-flex">
+								<div className="table">
+									<h3 className=" heading-light">employment status</h3>
+									<p className="para">Employed</p>
+								</div>
+								<div className="table">
+									<h3 className=" heading-light">Monthly income</h3>
+									<p className="para">₦200,000.00- ₦400,000.00</p>
+								</div>
+							</div>
+							<div className="main-content_box-details col-flex">
+								<div className="table">
+									<h3 className=" heading-light">sector of employment</h3>
+									<p className="para">FinTech</p>
+								</div>
+								<div className="table">
+									<h3 className=" heading-light">loan repayment</h3>
+									<p className="para">40,000</p>
+								</div>
+							</div>
+							<div className="main-content_box-details col-flex">
+								<div className="table">
+									<h3 className=" heading-light">Duration of employment</h3>
+									<p className="para">2 years</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="main-content_box col-flex">
+						<h2 className="heading-bold">Socials</h2>
+
+						<div className="contain row-flex">
+							<div className="main-content_box-details col-flex">
+								<div className="table">
+									<h3 className=" heading-light">Twitter</h3>
+									<p className="para">@grace_effiom</p>
+								</div>
+							</div>
+							<div className="main-content_box-details col-flex">
+								<div className="table">
+									<h3 className=" heading-light">Facebook</h3>
+									<p className="para">Grace Effiom</p>
+								</div>
+							</div>
+							<div className="main-content_box-details col-flex">
+								<div className="table">
+									<h3 className=" heading-light">Instagram</h3>
+									<p className="para">@grace_effiom</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="main-content_box col-flex">
+						<h2 className="heading-bold">Guarantor</h2>
+
+						<div className="contain row-flex">
+							<div className="main-content_box-details col-flex">
+								<div className="table">
+									<h3 className=" heading-light">full Name</h3>
+									<p className="para">Debby Ogana</p>
+								</div>
+							</div>
+							<div className="main-content_box-details col-flex">
+								<div className="table">
+									<h3 className=" heading-light">Phone Number</h3>
+									<p className="para">07060780922</p>
+								</div>
+							</div>
+							<div className="main-content_box-details col-flex">
+								<div className="table">
+									<h3 className=" heading-light">Email Address</h3>
+									<p className="para">debby@gmail.com</p>
+								</div>
+							</div>
+							<div className="main-content_box-details col-flex">
+								<div className="table">
+									<h3 className=" heading-light">Relationship</h3>
+									<p className="para">Sister</p>
 								</div>
 							</div>
 						</div>
