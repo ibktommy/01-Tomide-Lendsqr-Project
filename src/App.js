3c28981c325ed4af2d9060eb85980ec121c91e5f
ibktommy <atomquartz@gmail.com>
2022-07-03 12:07:13 +0100

    Separating each section into their separate pages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Login from './pages/Login/Login';
 import UserDashboard from './pages/UserDashboard/UserDashboard';
@@ -13,26 +14,36 @@ function App() {
   }
 
   preventSubmit()
+
   return (
-    <>
-      <Login />
-
-      <h1 className="top-header">
-        Users - General details
-      </h1>
-      <UsersGenDetails />
-
-      <h1 className='top-header'>
-        User - Dashboard
-      </h1>
-      <UserDashboard />
-
-      <h1 className="top-header">
-       Users - Showing Filters
-      </h1> 
-      <UserFilter />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='users-general-details' element={<UsersGenDetails />} />
+        <Route path='users-dashboard' element={<UserDashboard />} />
+        <Route path='users-filter' element={<UserFilter />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+// {/* <>
+//   <Login />
+
+//   <h1 className="top-header">
+//     Users - General details
+//   </h1>
+//   <UsersGenDetails />
+
+//   <h1 className='top-header'>
+//     User - Dashboard
+//   </h1>
+//   <UserDashboard />
+
+//   <h1 className="top-header">
+//     Users - Showing Filters
+//   </h1>
+//   <UserFilter />
+// </> */}
