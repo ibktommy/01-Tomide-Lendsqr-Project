fa1cabc88d6d4825c26eab7f96817b3f53316740
ibktommy <atomquartz@gmail.com>
2022-07-03 16:03:37 +0100

    Modifying the styles and structure of the User Dashboard

--- src/pages/LoginError/LoginError.jsx
+++ src/pages/LoginError/LoginError.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./LoginError.scss";
 import errorImage from "../../images/login/error.jpeg";
 
@@ -11,6 +12,13 @@ const LoginError = () => {
 						YOU NEED TO ENTER ADMIN LOGIN DETAILS TO PROCEED TO ANOTHER PAGE!
 					</h4>
 					<img src={errorImage} alt="error" className="error-content-img" />
+
+					<button className="error-btn">
+						<Link to="/">
+							<i className="fa-solid fa-left-long"></i>
+							Back to Login
+						</Link>
+					</button>
 				</div>
 			</section>
 		</>
